a01264b172c142bfa798cae4e5178718c6bbf187
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-01-25 00:31:10 +0530

    Update Navbar and Core components

--- client/src/Pages/Core.jsx
+++ client/src/Pages/Core.jsx
@@ -3,12 +3,33 @@ import { useState, useEffect } from "react";
 import Axios from "axios";
 import "./Core.css";
 import Card from "../Components/card";
+import PopupForm from "../Components/PopupForm";
 
 function Core() {
-  const [transaction, setTransaction] = useState("");
-  const [transactionType, setTransactionType] = useState("Expense");
-  const [category, setCategory] = useState("Food");
-  const [amount, setAmount] = useState(0);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleFormSubmit = (transaction, transactionType, category, amount) => {
+    // Do something with transaction and transactionType
+    console.log("handleSubmit called");
+    console.log("transaction " + transaction);
+    console.log("transactionType " + transactionType);
+    console.log("category " + category);
+    console.log("amount " + amount);
+    Axios.post("http://localhost:3001/createUser", {
+      Transaction: transaction,
+      TransactionType: transactionType,
+      Catagory: category,
+      Amount: amount,
+    })
+      .then((response) => {
+        // After the transaction is added, re-fetch the transactions
+        Axios.get("http://localhost:3001/getUsers").then((response) => {
+          setTransactions(response.data);
+        });
+        setAddPopupVisibility(false);
+      })
+      .catch((error) => console.error(error));
+  };
 
   const [isAddPopupVisible, setAddPopupVisibility] = useState(false);
   const [transactions, setTransactions] = useState([]);
@@ -53,93 +74,13 @@ function Core() {
     setAddPopupVisibility(!isAddPopupVisible);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("handleSubmit called");
-    console.log("transaction " + transaction);
-    console.log("transactionType " + transactionType);
-    console.log("category " + category);
-    console.log("amount " + amount);
-    Axios.post("http://localhost:3001/createUser", {
-      Transaction: transaction,
-      TransactionType: transactionType,
-      Catagory: category,
-      Amount: amount,
-    })
-      .then((response) => {
-        // After the transaction is added, re-fetch the transactions
-        Axios.get("http://localhost:3001/getUsers").then((response) => {
-          setTransactions(response.data);
-        });
-        setAddPopupVisibility(false);
-      })
-      .catch((error) => console.error(error));
-  };
-
   return (
     <>
-      {isAddPopupVisible && (
-        // In your form
-        <form className="popup" onSubmit={handleSubmit}>
-          <label>
-            <h2>Add Transaction</h2>
-            Transaction:
-            <input
-              type="text"
-              onChange={(e) => {
-                setTransaction(e.target.value);
-              }}
-            />
-            <br />
-            <br />
-            Transaction Type:
-            <select
-              value={transactionType}
-              onChange={(e) => {
-                setTransactionType(e.target.value);
-                console.log(e.target.value);
-              }}
-            >
-              <option value="Income">Income</option>
-              <option value="Expense">Expense</option>
-            </select>
-          </label>
-          <br />
-          <label>
-            Catagory:
-            <select
-              value={category}
-              defaultValue="Food"
-              onChange={(e) => {
-                setCategory(e.target.value);
-              }}
-            >
-              <option value="Food">Food</option>
-              <option value="Living">Living</option>
-              <option value="Entertainment">Entertainment</option>
-              <option value="Transportation">Transportation</option>
-              <option value="Education">Education</option>
-              <option value="Health">Health</option>
-              <option value="Other">Other</option>
-              <option value="Wage">Wage</option>
-              <option value="Gift">Gift</option>
-              <option value="Other">Other</option>
-            </select>
-          </label>
-          <br />
-          <label>
-            Amount:
-            <input
-              type="number"
-              onChange={(e) => {
-                setAmount(e.target.value);
-              }}
-            />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-      )}
+      <PopupForm
+        onSubmit={handleFormSubmit}
+        isOpen={isPopupOpen}
+        onClose={() => setIsPopupOpen(false)}
+      />
       <div className="div-grid">
         <div className="card card1">
           <Card className="greenGradient h-100 wallet-card">
@@ -162,7 +103,7 @@ function Core() {
               Rs. {sumOfIncomes.toLocaleString()}
             </p>
           </Card>
-          <button onClick={toggleAddPopup} className="add-button">
+          <button onClick={() => setIsPopupOpen(true)} className="add-button">
             Add Transaction
           </button>
         </div>
